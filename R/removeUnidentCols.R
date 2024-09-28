e6453c29ea77270da99c4169e0d87562b19be533
Henry Wyneken <hwyneken@github.com>
2024-09-28 09:47:30 -0500

    documented removeUnidentCols

--- R/removeUnidentCols.R
+++ R/removeUnidentCols.R
@@ -1,5 +1,45 @@
 #' removUnidentCols
 #' 
+#' \code{removeUnidentCols} removes variables from the selected set whose full linear targets would not be identifiable in the confirmation data.
+#' 
+#' @param selectedSet: The set of selected variables which SOIL scores above the cutoff. Selected variables are represented by their index - so if the column names are c("fizz","buzz"), and "fizz" was selected in step 3, then selectedSet = c(1).
+#' @param xCon: The X matrix of the confirmatory data. Dimension should be nCon * p.
+#' 
+#' @details{
+#' This function removes two types of unidentifiable variables from selectedSet.
+#' The first type is easier to explain - simply put, we check for variables which have practically zero variance
+#'  in the confirmation set.
+#' In the second step, we check for variables with linearly dependent relationships using the pracma package.
+#' 
+#' We start the second step by selecting the columns in xCon that correspond to the remaining variables
+#'  in selectedSet.
+#' Then, we check for linear dependencies.
+#' Specifically, we use the \code{pracma::rref} function, which returns a reduced row echelon matrix.
+#' 
+#' If the data is p-dimensional, the RREF matrix is an upper-triangular matrix. 
+#' If there are linear dependencies amongst the columns, some of the diagonal entries will be zero.
+#' A small practical example will help to explain this.
+#' 
+#' Suppose X <- cbind(c(1,1,0),c(0,0,1),c(1,1,1)).
+#' Therefore, the third column is just the sum of the first and second columns.
+#' The output from \code{pracma::rref(X)} is:
+#' 
+#' rref(X)
+#'      [,1] [,2] [,3]
+#; [1,]    1    0    1
+#' [2,]    0    1    1
+#' [3,]    0    0    0
+#' 
+#' The leading 1's on the diagonal indicate that we will keep the first two columns.
+#' However, the third column can be written as a sum of the first and second columns, 
+#'  as shown by the value of the third column: (c(1,1,0)).
+#' The final step of this function is to filter out all of the variables which do not have a 
+#'  diagonal value of "1" in the RREF matrix, and return the filtered selectedSet.
+#'  
+#' }
+#' 
+#' @return A (possibly reduced) copy of selected set, with unidentifiable variables removed.
+#' 
 #' @export
 
 
@@ -9,19 +49,6 @@ removeUnidentCols <- function(selectedSet,xCon) {
   sdInCon <- apply(xCon[,selectedSet],2,sd)
   whichHaveZeroSD <- which(sdInCon < 1e-16)
   selectedSet <- setdiff(selectedSet,selectedSet[whichHaveZeroSD]) # remove these
-  
-  ## now remove columns that are highly unbalanced
-  # why do we need this again
-  # don't forget to add mcvThresh back in as an argument
-  # mostCommonValueFraction <- function(x) {
-  #   valCounts <- table(x)
-  #   mostCommonInd <- which.max(valCounts)
-  #   mostCommonFraction <- valCounts[mostCommonInd] / sum(valCounts,na.rm=TRUE)
-  #   return(mostCommonFraction)
-  # }
-  # mostCommonFractionInCon <- apply(xCon[,selectedSet],2,mostCommonValueFraction)
-  # whichAreOverMCVThresh <- which(mostCommonFractionInCon >= mcvThresh)
-  # selectedSet <- setdiff(selectedSet,selectedSet[whichAreOverMCVThresh])
   
   # now check for dependent columns
   fullSelectedX <- xCon[,selectedSet] # we just need to make sure that we can fit models on the confirmation set
